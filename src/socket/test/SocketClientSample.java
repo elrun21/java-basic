581694ee2a03e42b14e4a781213a42596a200684
JooHyeong <ellion2121@gmail.com>
2022-06-11 09:48:44 +0900

    - 채팅 코드 샘플 반영

--- src/socket/test/SocketClientSample.java
+++ src/socket/test/SocketClientSample.java
@@ -7,48 +7,54 @@ import java.util.Scanner;
 
 public class SocketClientSample {
     static Scanner scanner;
+    Socket socket = null;
+    BufferedReader in = null;
+    BufferedWriter out = null;
+    OutputStream outputStream = null;
+    InputStream inputStream = null;
     public static void main(String[] args) {
         SocketClientSample sample = new SocketClientSample();
         scanner = new Scanner(System.in);
-        sample.sendSocketSample();
+        try {
+            sample.sendSocketSample();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void sendSocketSample() {
-        for (int loop = 0; true; loop++) {
+    private void sendSocketSample() throws IOException {
+        while(true){
             System.out.println("문자를 입력하세요");
             String request = scanner.nextLine();
+            if( request.equals("exit")) break;
             sendSocketData(request);
         }
-//        sendSocketData("EXIT");
+        inputStream.close();
+        outputStream.close();
+        out.close();
+        in.close();
+        socket.close();
+
     }
 
     private void sendSocketData(String data) {
-        Socket socket = null;
+
         try {
             System.out.println("Client:connecting");
             socket = new Socket("127.0.0.1", 9999);
-            System.out.println("Client:Connect status =" + socket.isConnected());
             Thread.sleep(1000);             // 1sec 대기
-            OutputStream stream = socket.getOutputStream(); //보낼 스트림
-            BufferedOutputStream out = new BufferedOutputStream(stream);
-            byte[] bytes = data.getBytes();     // 네트워크 통신은 byte
-            out.write(bytes);
+            outputStream = socket.getOutputStream(); //보낼 스트림
+            out = new BufferedWriter( new OutputStreamWriter( outputStream ) );
+            out.write(data);
+            out.newLine();
             out.flush();
-            System.out.println("Client:sent data: " + data);
+            System.out.println("sent data : " + data);
 
+            inputStream = socket.getInputStream();       // 앞으로 받을 데이타의 대한 파일 프라임
+            in = new BufferedReader(new InputStreamReader( inputStream) );
+            String recv = in.readLine();
+            System.out.println("recive Data : "+recv);
 
-            InputStream inputStream = socket.getInputStream();       // 앞으로 받을 데이타의 대한 파일 프라임
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(inputStream));
-            String temp = inputStream.toString();
-            if(!(temp.equals("java.net.SocketInputStream@4cdbe50f"))) {
-                int rev = (int) in.read();
-//                String rev = in.readLine();
-                System.out.println(rev);
-            }
-
-//            in.close();
-            out.close();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
